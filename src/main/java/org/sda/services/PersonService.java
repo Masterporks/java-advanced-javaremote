e70422c7f1fc9b62934306c96400ed3a45484b20
root <joosepkorela@gmail.com>
2022-10-27 20:17:48 +0300

    Interface examples added

---
+++ src/main/java/org/sda/services/PersonService.java
@@ -0,0 +1,40 @@
+package org.sda.services;
+
+
+import org.sda.models.Person;
+
+import java.time.LocalDate;
+
+/**
+ * PersonService interface to handle PErson related operations
+ *
+ * @author Joosep Korela
+ */
+public interface PersonService {
+    //Static field is allowed but not the normal field
+    static final int AVERAGE_AGE = 20;
+
+    /**
+     * To get the birth year of a Person
+     *
+     * @param age Age of a person
+     * @return Birth year
+     */
+    int getPersonBirthYear(int age);
+
+    /**
+     *
+     * To get full name the person
+     * @param person Person model
+     * @return Full name as String
+     */
+    String getPersonFullName(Person person);
+
+
+    //Static method definition is allowed
+    static int getAverageAge(){
+        return AVERAGE_AGE;
+    }
+
+
+}
